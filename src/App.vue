f8ae351c99383b89bd738a37f8b277929a2694d5
DarioRossi99 <rssdra99@gmail.com>
2022-12-06 15:20:58 +0100

    inserimento componenti

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script>
   import Header from './components/Header.vue';
   import Main from './components/Main.vue';
+  import CharactersList from './components/CharactersList.vue';
+  import SingleCharacter from './components/SingleCharacter.vue';
 
   export default{
     components: { Header,Main}
